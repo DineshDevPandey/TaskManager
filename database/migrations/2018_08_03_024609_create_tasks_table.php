462b99ebcbfb111f80907d7b967230915af60894
Dinesh Dev Pandey <pandey@psinc.jp>
2018-08-03 16:35:12 +0900

    table schema modified

--- database/migrations/2018_08_03_024609_create_tasks_table.php
+++ database/migrations/2018_08_03_024609_create_tasks_table.php
@@ -14,8 +14,10 @@ class CreateTasksTable extends Migration
     public function up()
     {
         Schema::create('tasks', function (Blueprint $table) {
-            $table->increments('id');
-            $table->timestamps();
+          $table->increments('id');
+          $table->string('title');
+          $table->text('description');
+          $table->timestamps();
         });
     }
 
